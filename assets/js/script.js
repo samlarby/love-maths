eb16a6d323440d8ad0bc155a4b17c8ae8cabf58f
Sam Larby <samlarby1997@hotmail.com>
2024-02-22 19:27:53 +0000

    adding alerts to the buttons

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+console.log("connected")
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button"); 
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit!"); 
+            }
+            else {
+                let gameType = this.getAttribute("data-type"); 
+                alert(`You clicked ${gameType}`); 
+            }
+        });
+    }
+});
+
+
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+    
+}
+
+function incrementWrongAnswer(){
+    
+}
+
+function displayAdditionQuestion(){
+    
+}
+
+function displaySubtractQuestion(){
+    
+}
+
+function displayMultiplyQuestion(){
+    
+}
